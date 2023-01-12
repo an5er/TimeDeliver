03ad00fb7cd74d5e5aa41dfa1d42dfadc14ef705
an5er <1318861442@qq.com>
2023-01-12 22:03:57 +0800

    update home

---
+++ web/src/view/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <particles-component/>
+</template>
+
+<script>
+import ParticlesComponent from "../../components/particles/index.vue";
+export default {
+  name: "test",
+  components: {ParticlesComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
